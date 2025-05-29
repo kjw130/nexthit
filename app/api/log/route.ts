0fa9e8626697d715aa238d7fb82b253e13212b01
Kai Ward <wardkai558@gmail.com>
2025-05-29 03:35:45 +0100

    Update package.json

--- app/api/log/route.ts
+++ app/api/log/route.ts
@@ -1,6 +1,7 @@
 // app/api/log/route.ts
-import { PrismaClient } from '@prisma/client';
+
 import { NextRequest, NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
